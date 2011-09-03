94cfc8b6e6c3c135e7e463019260ac6757cb183f
Simon Watiau <simon@Simons-MacBook-Pro.local>
2011-09-03 16:55:21 -0700

    Initial Commit

---
+++ Stories/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Stories
+//
+//  Created by Simon Watiau on 9/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
---
+++ Stories/StoriesAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  StoriesAppDelegate.h
+//  Stories
+//
+//  Created by Simon Watiau on 9/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoriesAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
